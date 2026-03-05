bfd100593c0792868c603972d250ddc6eac7e7dc
6Joey6 <joeytan.workinguse@gmail.com>
2026-03-05 15:05:13 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
+# utils.py
 import pandas as pd
 import os
 
 def load_data():
-    """Load dataset.csv and convert important columns to numeric."""
+    """Load dataset.csv and convert important columns to numeric"""
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(BASE_DIR, "dataset.csv")
 
     df = pd.read_csv(file_path)
 
-    # Columns that need to be numeric
+    # Columns to convert
     numeric_cols = [
         "ICU","INTUBATED","DIABETES","COPD","ASTHMA","INMUSUPR",
-        "HYPERTENSION","CARDIOVASCULAR","OBESITY","CHRONIC_KIDNEY","TOBACCO"
+        "HYPERTENSION","CARDIOVASCULAR","OBESITY","CHRONIC_KIDNEY","TOBACCO",
+        "HOSPITALIZED","PNEUMONIA","PREGNANCY","ANOTHER CASE"
     ]
 
     for col in numeric_cols:
