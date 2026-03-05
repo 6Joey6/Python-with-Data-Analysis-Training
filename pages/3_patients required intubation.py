2764dbbc2c7a1d64a87bcd3261246a4cd817ec51
6Joey6 <joeytan.workinguse@gmail.com>
2026-03-05 15:05:57 +0800

    Enhance patients intubation report with visualization
    Updated the intubation page to include a donut chart and display counts of patients requiring intubation.

--- pages/3_patients required intubation.py
+++ pages/3_patients required intubation.py
@@ -1,12 +1,22 @@
 # pages/3_Intubation.py
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 from utils import load_data
 
-st.title("Question 3: Patients Requiring Intubation")
+st.title("Q3: Patients Requiring Intubation")
 
 df = load_data()
 
-# Count INTUBATED = 1 (YES)
-intubated_count = df[df["INTUBATED"] == 1].shape[0]
+# Count yes/no
+intubated_counts = df["INTUBATED"].map({1:"YES", 2:"NO"}).value_counts()
 
-st.metric("Patients requiring intubation", intubated_count)
+# Donut chart
+fig = px.pie(
+    names=intubated_counts.index,
+    values=intubated_counts.values,
+    title="Proportion of Patients Requiring Intubation",
+    hole=0.4
+)
+st.plotly_chart(fig)
+st.dataframe(intubated_counts)
