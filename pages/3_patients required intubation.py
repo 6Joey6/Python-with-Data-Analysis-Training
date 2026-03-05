fb8985eb84f03ae02dda3c16fb02c77f51357126
6Joey6 <joeytan.workinguse@gmail.com>
2026-03-05 15:10:26 +0800

    Update 3_patients required intubation.py

--- pages/3_patients required intubation.py
+++ pages/3_patients required intubation.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from utils import load_data
+import io
 
 st.title("Q3: Patients Requiring Intubation")
 
@@ -20,3 +21,16 @@ fig = px.pie(
 )
 st.plotly_chart(fig)
 st.dataframe(intubated_counts)
+
+csv_buffer = io.StringIO()
+data_to_download = cross_tab  # replace with the DF you want to allow download
+data_to_download.to_csv(csv_buffer, index=False)
+
+# Add download button
+st.download_button(
+    label="Download Report as CSV",
+    data=csv_buffer.getvalue(),
+    file_name="q3_intubation_report.csv",  # replace qX with question number
+    mime="text/csv"
+)
+
